78b9dd03644388a819866d3498d1a086926e6433
Eric Lemieux <eric@lemieuxdev.com>
2021-03-04 21:40:49 -0500

    Replace log echos with printf for more portability
    Also adds heredoc example for the help command.

--- template.sh
+++ template.sh
@@ -7,15 +7,21 @@ test "${DEBUG:-0}" = "1" && set -x
 
 # Logging utilities
 function log::_timestamp() { date +'%Y-%m-%dT%H:%M:%S%z'; }
-function log::info() { echo "$(log::_timestamp) [INFO] $*" >&2; }
-function log::warn() { echo "$(log::_timestamp) [WARN] $*" >&2; }
-function log::error() { echo "$(log::_timestamp) [ERROR] $*" >&2; }
+function log::info() { printf "$(log::_timestamp) [INFO] $*\n" >&2; }
+function log::warn() { printf "$(log::_timestamp) [WARN] $*\n" >&2; }
+function log::error() { printf "$(log::_timestamp) [ERROR] $*\n" >&2; }
 
 # Display help information about your script, can include description of what the script does, what arguments it
 # accepts, any requirements, etc.
 # Remove if you have no need for a help message.
 function help() {
-    echo "Some helpful information about how to use your script"
+    cat<<EOF
+Some helpful information about how to use your script.
+
+Maybe include what flags you can use.
+
+-h | --help   Show this help message.
+EOF
     exit 0
 }
 
